c25ece8d4b781f699e7f3d94af43be6f5c7118e6
hyjing <frankjinghaoyu@gmail.com>
2019-11-11 22:16:55 -0500

    Create home page for user and login page, implement search functionality

--- webserver/server.py
+++ webserver/server.py
@@ -108,7 +108,7 @@ def index():
   """
 
   if session.get('user_id'):
-    return redirect("/types")
+    return redirect("/home")
   return render_template("index.html")
 
 @app.route('/types', methods=['GET', 'POST'])
@@ -144,7 +144,6 @@ def book():
     return redirect("/types")
   
   tid = session['tid'].encode("utf-8")
-  print("tid is: ", tid)
   posts = g.conn.execute(
       "SELECT B.isbn, B.title, B.date, B.outline FROM book B, booktype BT"
       " WHERE BT.tid = {} AND B.isbn = BT.isbn;".format(tid)
@@ -155,12 +154,16 @@ def book():
     return redirect("/bookContent")
   return render_template("book.html", posts=posts)
 
-@app.route('/liketype', methods=['POST'])
-def liketype():
-  tid = request.form['tid']
-  uid = request.form['uid']
-  g.conn.execute('INSERT INTO likebook(tid, uid) VALUES (%d, %d);', tid, uid)
-  return redirect("/book")
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+  posts = []
+  if request.method == 'POST':
+    isbn_search = request.form["isbn_search"]
+    posts = g.conn.execute(
+      "SELECT B.isbn, B.title, B.date, B.outline FROM book B"
+      " WHERE B.isbn = {};".format(isbn_search)
+    ).fetchall()
+  return render_template("/search", posts=posts)
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
@@ -181,7 +184,7 @@ def login():
         session.clear()
         session['user_id'] = user['uid']
         # session['user_name'] = user['last_name'] + ' ' + user['first_name']
-        return redirect('/types')
+        return redirect('/home')
 
     flash(error)
 
